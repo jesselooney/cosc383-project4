c9b009d8ab037184c1022fc0d426b50493d097bd
BattleCh1cken <BattleCh1cken@larkov.de>
2024-11-02 08:22:33 -0400

    wrote a test case to cover the rest of the api

--- tests/test_bit_patterns.py
+++ tests/test_bit_patterns.py
@@ -1,5 +1,5 @@
 import random
-from stegtools.bit_patterns import transject
+from stegtools.bit_patterns import transject, inject, eject
 from bitarray import bitarray
 import imageio.v3 as iio
 import numpy as np
@@ -25,4 +25,20 @@ def test_transject_invertible():
 
     assert np.array_equal(new_img, img), 'double transjection should be an identity'
     assert new_msg == msg, 'double transjection should be an identity'
+
+def test_inject_eject_invertible():
+    img = iio.imread("imageio:chelsea.png")
+    
+    random.seed(0)
+    msg = bitarray([random.choice([True, False]) for _ in range(100_000)])
+
+    def pattern(row, col, chn, idx):
+        return True
+    
+    modified_img = inject(img, pattern, msg)
+    assert not np.array_equal(modified_img, img), 'injection should modify the image'
+
+    parsed_msg = eject(modified_img, pattern, 100_000)
+    assert parsed_msg == msg, 'Should be reversible'
+
 
