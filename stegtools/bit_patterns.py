5bb05969f786b2e2de8f70855d3c980c30810ffc
Jesse Looney <42242696+jesselooney@users.noreply.github.com>
2024-11-01 21:33:50 -0400

    finish bit pattern api

--- stegtools/bit_patterns.py
+++ stegtools/bit_patterns.py
@@ -1,69 +1,9 @@
 import numpy as np
 import imageio.v3 as iio
 from bitarray import bitarray
-import random
 
 
-def eject(image: np.ndarray, pattern: callable, count: int = None) -> bitarray:
-    message = bitarray()
-    it = np.nditer(image, flags=['multi_index'])
-
-    for value in it:
-        bits = bitarray(endian='little')
-        bits.frombytes(int(value).to_bytes())
-
-        for index, bit in enumerate(bits):
-            if count is not None and len(message) >= count:
-                return message
-            if pattern(*it.multi_index, index):
-                print(*it.multi_index, index, ' -> ', bit)
-                message.append(bit)
-        print(bits, value)
-
-    #error if requested count too long
-
-    return message
-
-
-def inject(image: np.ndarray, pattern: callable, message: bitarray):
-    it = np.nditer(image, flags=['multi_index'])
-
-    new_image = np.copy(image)
-    message_index = 0
-
-    for value in it:
-        bits = bitarray(endian='little')
-        bits.frombytes(int(value).to_bytes())
-
-        new_bits = bitarray(8, endian='little')
-        for i, bit in enumerate(bits):
-            new_bits[i] = bits[i]
-
-        for index, bit in enumerate(bits):
-            if message_index >= len(message):
-                break
-
-            if pattern(*it.multi_index, index):
-                print(*it.multi_index, index, ' <- ', message[message_index])
-                new_bits[index] = message[message_index]
-                message_index += 1
-
-        new_value = int.from_bytes(new_bits.tobytes())
-        print(new_bits, new_value)
-        new_image[*it.multi_index] = new_value
-
-        if message_index >= len(message):
-            return new_image
-
-    if message_index < len(message) - 1:
-        raise Exception(f'Message does not fit! Remaining bits: {(len(message) - 1) - message_index}')
-
-    return new_image
-
-def f(row, col, channel, index):
-    return row % 5 == 0 and col <= 10
-
-def transject(image: np.ndarray, message: bitarray, pattern: callable):
+def transject(image: np.ndarray, pattern: callable, message: bitarray) -> int:
     it = np.nditer(image, order='C', flags=['multi_index'])
 
     message_index = 0
@@ -74,10 +14,6 @@ def transject(image: np.ndarray, message: bitarray, pattern: callable):
 
         bits = bitarray(endian='little')
         bits.frombytes(int(subpixel).to_bytes())
-
-        #new_bits = bitarray(8, endian='little')
-        #for i, bit in enumerate(bits):
-        #    new_bits[i] = bits[i]
 
         for i, bit in enumerate(bits):
             if message_index >= len(message):
@@ -92,31 +28,23 @@ def transject(image: np.ndarray, message: bitarray, pattern: callable):
         image[*it.multi_index] = new_subpixel
 
     if message_index < len(message):
-        raise Exception(f'Length mismatch: {len(message)} message bits but only {message_index} suitable bits were found in cover image.')
+        print(f'WARNING: Length mismatch: {len(message)} message bits but only {message_index} suitable bits were found in cover image.')
 
-random.seed(0)
+    return message_index
 
-im = iio.imread('imageio:chelsea.png')
-msg = bitarray([True for i in range(50 * 10 * 24)])
-im2 = im.copy()
-msg2 = msg.copy()
 
-random.seed(0)
-transject(im2, msg2, f)
-print(np.array_equal(im2, im), msg2 == msg)
+def inject(image: np.ndarray, pattern: callable, message: bitarray) -> np.ndarray:
+    new_image = image.copy()
+    transject(new_image, pattern, message.copy())
+    return new_image
 
-iio.imwrite('test-out.png', im2)
 
-random.seed(0)
-transject(im2, msg2, f)
-print(np.array_equal(im2, im), msg2 == msg)
+def eject(image: np.ndarray, pattern: callable, length: int = -1) -> bitarray:
+    if length == -1:
+        length = image.size * 8
 
-#message = bitarray()
-#message.frombytes(bytes(random.randrange(256) for _ in range(2**10)))
-#random.seed(1)
-#im2 = inject(im, lsb, message)
-#iio.imwrite("test-out.png", im2)
-#print('-------')
-#random.seed(1)
-#out = eject(im2, lsb, len(message))
-#print(message == out)
+    message = bitarray(length)
+    true_length = transject(image.copy(), pattern, message)
+    return message[:true_length]
+
+
