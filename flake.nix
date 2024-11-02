7b0c16ee28feae1f3ca3bb94d04b57aa56285084
BattleCh1cken <BattleCh1cken@larkov.de>
2024-11-02 17:27:09 -0400

    hi you clearly need a flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs =
+        import nixpkgs
+        {
+          inherit system overlays;
+        };
+
+      packages = with pkgs; [
+        dbus.lib
+        dbus.dev
+
+        pkg-config
+        rust-bin.nightly.latest.default
+        rust-analyzer
+      ];
+    in {
+      devShell =
+        pkgs.mkShell
+        {
+          buildInputs = packages;
+          RUST_LOG = "trace";
+        };
+    });
+}
