1b99672439d0a83915adc66f69544c638911baaa
Jesse Looney <42242696+jesselooney@users.noreply.github.com>
2024-11-01 21:58:55 -0400

    write test

--- tests/test_bit_patterns.py
+++ tests/test_bit_patterns.py
@@ -1,5 +1,28 @@
+import random
 from stegtools.bit_patterns import transject
+from bitarray import bitarray
+import imageio.v3 as iio
+import numpy as np
 
 
 def test_transject_invertible():
-    image = iio.imread("imageio:chelsea.png")
+    img = iio.imread("imageio:chelsea.png")
+    
+    random.seed(0)
+    msg = bitarray([random.choice([True, False]) for _ in range(100_000)])
+
+    def pattern(row, col, chn, idx):
+        return True
+    
+    new_img = img.copy()
+    new_msg = msg.copy()
+    transject(new_img, pattern, new_msg)
+
+    assert not np.array_equal(new_img, img), 'transjection should modify the image'
+    assert new_msg != msg, 'transjection should modify the message'
+
+    transject(new_img, pattern, new_msg)
+
+    assert np.array_equal(new_img, img), 'double transjection should be an identity'
+    assert new_msg == msg, 'double transjection should be an identity'
+
