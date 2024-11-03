4bff569e9ad8a86fce97c494a693c18b818a0985
BattleCh1cken <BattleCh1cken@larkov.de>
2024-11-03 12:37:36 -0500

    it might actually work now

---
+++ src/bit_patterns.rs
@@ -0,0 +1,75 @@
+use bitvec::prelude::*;
+use image::{GenericImageView, ImageBuffer, Pixel, RgbImage};
+
+type Pattern = fn(u32, u32, usize, usize) -> bool;
+
+pub fn transject(mut image: RgbImage, pattern: Pattern, mut message: BitVec) -> (RgbImage, BitVec) {
+    /*
+    I need to
+    - iterate of the individual channels of the image
+    - check if the channel can be accessed with the pattern
+    - do the flipsy flipsy with the message
+    */
+
+    let mut message_index = 0;
+    for (pixel_row, pixel_col, pixel) in &mut image.enumerate_pixels_mut() {
+        for (channel_index, channel_value) in &mut pixel.channels_mut().into_iter().enumerate() {
+            let bits = channel_value.view_bits_mut::<Lsb0>();
+            for (bit_index, mut bit) in bits.iter_mut().enumerate() {
+                if message_index >= message.len() {
+                    break;
+                }
+
+                if pattern(pixel_row, pixel_col, channel_index, bit_index) {
+                    // i'm sure theres a better way to swap these two but idc
+                    let tmp = bit.clone();
+                    *bit = *message.get(message_index).unwrap();
+                    message.set(message_index, tmp);
+
+                    message_index += 1;
+                }
+            }
+        }
+    }
+    return (image, message);
+}
+
+pub mod patterns {
+    pub fn access_all(row: u32, column: u32, channel: usize, index: usize) -> bool {
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn transject_invertible() {
+        let img: RgbImage = image::open("assets/blahaj-in-bando.png").unwrap().into();
+
+        let mut message = bitvec![usize, LocalBits; 0; 40];
+        message[0..10].store::<u16>(0x3A8);
+        message[10..20].store::<u16>(0x2F9);
+        message[20..30].store::<u16>(0x154);
+        message[30..40].store::<u16>(0x06D);
+
+        let (modified_img, modified_message) =
+            transject(img.clone(), patterns::access_all, message.clone());
+
+        assert_ne!(modified_img, img);
+        assert_ne!(modified_message, message);
+
+        let (restored_img, restored_message) = transject(
+            modified_img.clone(),
+            patterns::access_all,
+            modified_message.clone(),
+        );
+
+        assert_eq!(restored_img, img);
+        assert_eq!(restored_message, message);
+    }
+
+    #[test]
+    fn inject_eject_reversible() {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,20 @@
 use std::time::SystemTime;
-
+mod bit_patterns;
+use bit_patterns::transject;
 use bitvec::prelude::*;
-use image::{GenericImageView, ImageBuffer, Pixel, RgbImage};
-
-fn transject(image: RgbImage, pattern: fn(u32, u32, usize, usize) -> bool, message: BitVec) {
-    /*
-    I need to
-    - iterate of the individual channels of the image
-    - check if the channel can be accessed with the pattern
-    - do the flipsy flipsy with the message
-    */
-    let mut message_index = 0;
-    for (row, col, pixel) in image.enumerate_pixels() {
-        if message_index >= message.len() {
-            break;
-        }
-
-        for (channel_index, channel_value) in pixel.channels().into_iter().enumerate() {
-            let bits = BitArray::<_, Msb0>::new(*channel_value);
-            for (bit_index, bit) in bits.into_iter().enumerate() {
-                if pattern(row, col, channel_index, bit_index) {
-                    message_index += 1;
-                };
-            }
-        }
-    }
-}
-
-fn inject() {}
-
-fn eject() {}
-
-fn access_all(row: u32, column: u32, channel: usize, index: usize) -> bool {
-    true
-}
 
 fn main() {
     let img = image::open("blahaj-in-bando.png").unwrap();
 
     let start = SystemTime::now();
-    let bits = bitvec![usize, LocalBits; 0; 40];
+
+    let mut bits = bitvec![usize, LocalBits; 0; 40];
+    bits[0..10].store::<u16>(0x3A8);
+    bits[10..20].store::<u16>(0x2F9);
+    bits[20..30].store::<u16>(0x154);
+    bits[30..40].store::<u16>(0x06D);
     println!("{:?}, {}", bits, bits.len());
-    transject(img.into(), access_all, bits);
+    //transject(img.into(), bit_patterns::patterns::access_all, bits);
     let end = SystemTime::now();
 
     let duration = end.duration_since(start).unwrap();
