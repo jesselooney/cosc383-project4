bb8cc5e8f163a96c307c5cbe33939ba41532b93a
Jesse Looney <42242696+jesselooney@users.noreply.github.com>
2024-11-11 15:53:10 -0500

    add write_raw

--- src/assemble.rs
+++ src/assemble.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use bitvec::prelude::*;
 use std::{fs, path::Path};
 
+/// Helper function to write each byte of BitVec to a file. Watch out for the bit order.
 fn write_raw(path: impl AsRef<Path>, bv: BitVec<u8>) -> Result<()> {
     fs::write(path, bv.as_raw_slice())?;
     Ok(())
